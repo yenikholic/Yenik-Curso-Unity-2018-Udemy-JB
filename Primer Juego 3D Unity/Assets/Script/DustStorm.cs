8c3bcf25850c7f85beac075a148a301bd8646c0d
yenikholic <46165642+yenikholic@users.noreply.github.com>
2018-12-26 20:30:53 +0100

    Primer Juego 3D Coin Collector
    asd

---
+++ Primer Juego 3D Unity/Assets/Script/DustStorm.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DustStorm : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player")) gameObject.GetComponent<ParticleSystem>().Play();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player")) gameObject.GetComponent<ParticleSystem>().Stop();
+    }
+}
---
+++ Primer Juego 3D Unity/Assets/Script/Timer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public float maxTime;
+    public static float countDown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        countDown = maxTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countDown -= Time.deltaTime;
+        if (countDown <= 0)
+        {
+            Debug.Log("Te has quedado sin tiempo... HAS PERDIDO!");
+
+            Coin.coinCount = 0;
+
+            SceneManager.LoadScene("Level_01");
+        }
+    }
+}
